f27c7e799c31c8761e22981d2d7649bc31700f6c
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-07-23 04:47:21 -0300

    Update de série implementado

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -16,15 +16,29 @@ class SeriesController
 
     public function store(Request $request)
     {
-        return response()->json(Serie::create(['nome' => $request->nome]), 201);
+        return response()->json(Serie::create($request->all()), 201);
     }
 
-    public function get(int $id)
+    public function show(int $id)
     {
         $serie = Serie::find($id);
         if (is_null($serie)){
             return response()->json('', 204);
         }
         return response()->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)){
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+                ], 404);
+        }
+        $serie->fill($request->all());
+        $serie->save();
+
+        return $serie;
     }
 }
